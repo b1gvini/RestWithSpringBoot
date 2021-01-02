c6dff85430e985a5ea69cf302c9c2231ac1bb588
Vinícius de França <vgf.vinicius@gmail.com>
2021-01-02 12:12:14 -0300

    Feliz ano novo/Teste

---
+++ rest-with-spring-boot/src/main/java/br/com/b1gvini/GreetingsController.java
@@ -0,0 +1,19 @@
+package br.com.b1gvini;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+	private static final String template = "FELIZ ANO NOVO, %s";
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	public Greetings greeting(@RequestParam(value="name", defaultValue="Vinicius!!")String name) {
+		return new Greetings(counter.incrementAndGet(),String.format(template, name));
+	}
+}
